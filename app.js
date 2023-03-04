7dc95682146246d12d381372d4c144d3c62d792b
AlinaShahmur <ashaturnaya@gmail.com>
2023-03-04 16:07:40 +0200

    Added logs

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
+
+console.log(process.env.NODE_ENV);
+console.log("dirname:" + __dirname);
 if (process.env.NODE_ENV === 'production') {
+    console.log("inside");
     app.use('/', express.static(path.join(__dirname, 'dist')))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
     })
 }
 
-app.listen(process.env.PORT||3000, () => console.log(`listening on port 8000`));
+app.listen(process.env.PORT||3000, () => console.log(`listening on port 3000`));
