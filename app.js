cb26a09e77480feec33e5e30935bb0825850d22c
AlinaShahmur <ashaturnaya@gmail.com>
2023-03-04 15:47:19 +0200

    Changed port

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-app.listen(process.env.PORT || 3000, () => console.log(`listening on port 8000`));
+app.listen(3000, () => console.log(`listening on port 8000`));
